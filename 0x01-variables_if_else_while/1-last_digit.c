10d3a06cf958966f80eb65c8145d32ab9a587323
khadijach2 <charef2.khadija@gmail.com>
2023-06-16 18:13:29 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /*
  * main -Entry point
- * Description: 'get the last digit of a number'
+ * Description: get the last digit of a number
  * Return: Always 0
  */
 int main(void)
