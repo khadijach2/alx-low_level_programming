3d585358ff18ed87f7174be013cf15e39df95dbe
khadijach2 <charef2.khadija@gmail.com>
2023-07-10 17:25:07 -0700

    I will show you how great I a

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -5,17 +5,17 @@
 /**
  * print_tab - Prints an array of string
  * @tab: The array to print
- *
  * Return: nothing
  */
+
 void print_tab(char **tab)
 {
-    int i;
+	int i;
 
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
+	for (i = 0; tab[i] != NULL; ++i)
+	{
+		printf("%s\n", tab[i]);
+	}
 }
 
 /**
@@ -25,14 +25,14 @@ void print_tab(char **tab)
  */
 int main(void)
 {
-    char **tab;
+	char **tab;
 
-    tab = strtow("      ALX School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
+	tab = strtow("      ALX School         #cisfun      ");
+	if (tab == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	print_tab(tab);
+	return (0);
 }
